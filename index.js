73333ed479715163b67f6556d34e84ad33a6c820
aberg07 <140882154+aberg07@users.noreply.github.com>
2024-02-20 12:12:27 +0900

    fix: Installed necessary packages

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,6 +13,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+})
+let user = mongoose.model('user', userSchema);
+
+app.post('/api/users', async (req, res) => {
+  let userInput = req.body.username;
+  let userSearch = await user.findOne({username: userInput})
+  //If an already existing user name is entered, return that user
+  if (userSearch) {
+    res.json(userSearch)
+  }
+  //Otherwise create a new user entry in the database
+  else {
+    newUser = new user({username: userInput});
+    await newUser.save();
+    console.log(newUser);
+    res.json(newUser)
+  }
+})
 
 
 
